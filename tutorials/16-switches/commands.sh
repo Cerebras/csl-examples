50e45ff70605185ae94cc2f7682e2f19e24902f0
leightonw-cb <leighton.wilson@cerebras.net>
2023-05-10 12:20:10 -0700

    clean up examples for SDK version 0.7.0

--- tutorials/16-switches/commands.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-cslc ./code.csl --fabric-dims=5,5 --fabric-offsets=1,1 -o out
-cs_python run.py --name out
--- tutorials/21-collectives/commands.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-cslc ./layout.csl --fabric-dims=15,15 --fabric-offsets=0,0 \
---params=Pw:15,Ph:15,chunk_size:3 -o out
-cs_python run.py --name out
