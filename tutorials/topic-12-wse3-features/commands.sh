674180fdc4af8cbb3a623098d84a8c9ef40da7a4
leightonw-cb <leighton.wilson@cerebras.net>
2024-04-14 19:26:54 -0700

    update switch syntax and add spmv data

--- tutorials/topic-12-wse3-features/commands.sh
+++ tutorials/topic-12-wse3-features/commands.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-cslc ./layout.csl --fabric-dims=11,3 \
+cslc --arch=wse3 ./layout.csl --fabric-dims=11,3 \
 --fabric-offsets=4,1 -o out --memcpy --channels 1
 cs_python run.py --name out
